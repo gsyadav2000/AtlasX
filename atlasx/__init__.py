f8b67f24ca73daccce7069ad4913503beafae91e
gsyadav2000 <ygs92829@gmail.com>
2026-07-22 17:23:54 +0530

    Configure AtlasX as an installable Python package

---
+++ atlasx/__init__.py
@@ -0,0 +1,14 @@
+"""
+AtlasX
+
+A Python toolkit for genomic peak annotation
+and downstream analysis.
+"""
+
+__version__ = "0.6.0"
+
+__author__ = "Ghanshyam Yadav"
+
+__email__ = ""
+
+__license__ = "MIT"
